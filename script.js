05b8eb9edc7b4622991aebf90395d2d7c46e5aa2
Maheshbilla03 <maheshbilla03@gmail.com>
2026-06-09 16:04:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSignup(){
-  document.getElementById("loginBox").classList.remove("active");
-  document.getElementById("signupBox").classList.add("active");
-}
-
-function showLogin(){
-  document.getElementById("signupBox").classList.remove("active");
-  document.getElementById("loginBox").classList.add("active");
-}
